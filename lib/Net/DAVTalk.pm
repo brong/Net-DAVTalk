7b8e9b3ea30989efaa30691d93041115858beab2
ellie timoney <ellie@fastmail.com>
2025-01-15 12:00:16 +1100

    DAVTalk: pass SSL_options through to HTTP::Tiny

--- lib/Net/DAVTalk.pm
+++ lib/Net/DAVTalk.pm
@@ -86,6 +86,8 @@ Options:
 
     headers: a hashref of additional headers to add to every request
 
+    SSL_options: a hashref of SSL options to pass down to the default
+    user agent
 =cut
 
 # General methods
@@ -141,6 +143,7 @@ sub ua {
   else {
     $Self->{ua} ||= HTTP::Tiny->new(
       agent => "Net-DAVTalk/$VERSION",
+      SSL_options => $Self->{SSL_options},
     );
   }
   return $Self->{ua};
